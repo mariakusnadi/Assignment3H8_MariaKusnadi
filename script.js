fb86323b8ca068949a52cbc1f9b7465b3c73148c
mariakusnadi <maria@mariakusnadi.com>
2021-10-28 21:36:56 +0700

    menambahkan function untuk mengambil dan mengolah data hasil fetch

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-fetch('https://indonesia-covid-19.mathdro.id/api')
-.then(response => response.json ())
-.then(data => console.log(data))
+function renderData(jumlahKasus){
+    let tableCasesEl = document.getElementById("tableCases")
+    jumlahKasus.forEach(kasus => {
+        tableCasesEl.innerHTML += `
+        <tr>
+            <th scope="row">${kasus.provinsi}</th>
+            <td>${kasus.kasusPosi}</td>
+            <td>${kasus.kasusSemb}</td>
+            <td>${kasus.kasusMeni}</td>
+        </tr>
+        `
+    });
+}
+
+function jumlahPositif(jumlahKasus){
+    let hasilPositif = 0
+    jumlahKasus.forEach(kasus => {
+        hasilPositif += Number(kasus.kasusPosi) 
+    });
+    document.getElementById("positif").innerHTML = `Positif: ${hasilPositif}`
+}
+
+function jumlahSembuh(jumlahKasus){
+    let hasilSembuh = 0
+    jumlahKasus.forEach(kasus => {
+        hasilSembuh += Number(kasus.kasusSemb)
+    });
+    document.getElementById("sembuh").innerHTML = `Sembuh: ${hasilSembuh}`
+}
+
+function jumlahMeninggal(jumlahKasus){
+    let hasilMeninggal = 0
+    jumlahKasus.forEach(kasus => {
+        hasilMeninggal += Number(kasus.kasusMeni)
+    });
+    document.getElementById("meninggal").innerHTML = `Meninggal: ${hasilMeninggal}`
+}
+
+fetch('https://indonesia-covid-19.mathdro.id/api/provinsi')
+    .then(response => response.json ())
+    .then(kasus => kasus.data)
+    .then(jumlahKasus => {
+        renderData(jumlahKasus)
+        jumlahPositif(jumlahKasus)
+        jumlahSembuh(jumlahKasus)
+        jumlahMeninggal(jumlahKasus)
+    })
